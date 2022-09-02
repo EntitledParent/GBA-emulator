dbaeb039cde00fa73709cada242f6e026bea1580
Logan <thecrazykid2121@gmail.com>
2022-09-02 07:36:15 -0400

    fix??

--- user_scripts/abcloak.js
+++ user_scripts/abcloak.js
@@ -1,14 +1,2 @@
-let test = 1;
-function toggleCheck() {
-    if(document.getElementById("abtog").checked === true){
-    test = 2;
-    localStorage.setItem("testItem", JSON.stringify(test));
-    console.log("checked");
-    }
-    else{
-        test = 1;
-        localStorage.setItem("testItem", JSON.stringify(test));
-        console.log("unchecked");
-    }
-}
+
 
