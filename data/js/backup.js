cfc43b033ed9cd235890f4c18fef69382d427338
Logan <thecrazykid2121@gmail.com>
2022-10-06 22:38:31 -0400

    backup sys hide

--- data/js/backup.js
+++ data/js/backup.js
@@ -1,15 +1,15 @@
-fetch('http://endpoint.i10.repl.co/nes-alt/Abadox.nes.zip', {mode: 'no-cors'}).then(r=>{
-  console.log("Server OK.");
-  })
-  .catch(e=>{
-    serverOffline();
-    });
+//fetch('http://endpoint.i10.repl.co/nes-alt/Abadox.nes.zip', {mode: 'no-cors'}).then(r=>{
+  //console.log("Server OK.");
+ // })
+  //.catch(e=>{
+  //  serverOffline();
+  //  });
 
 
 
-function serverOffline() {
-    let serverStatus = 1;
-    alert("The main server is blocked, or offline. Loading alternative server (BETA FEATURE STILL IN DEV STAGE)...");
-    localStorage.setItem("serverStat", JSON.stringify(serverStatus));
-    console.log(serverStatus);
-}
+//function serverOffline() {
+ //   let serverStatus = 1;
+  //  alert("The main server is blocked, or offline. Loading alternative server (Only supported for GBA/NES Games if you don't think you should have this message, please reload the page once or twice to fix.)");
+  //  localStorage.setItem("serverStat", JSON.stringify(serverStatus));
+   // console.log(serverStatus);
+//}
